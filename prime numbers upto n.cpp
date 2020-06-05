ff905d9a65e53430955f08dbd85595ae91f37682
Adarsh <42761058+adarsh1github1@users.noreply.github.com>
2020-06-05 20:59:45 +0530

    seive eratosthenes method

---
+++ prime numbers upto n.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void seiveofEratosthenes(int n){
+	bool prime[n+1];
+	memset(prime,true,sizeof(prime));
+	for(int p=2;p*p<=n;p++){
+		if(prime[p] == true){
+			for(int i=p*p;i<=n;i+=p)
+				prime[i] = false;
+		}
+	}
+	for(int i=2;i<=n;i++)
+		if(prime[i] == true)
+			cout<<i<<" ";
+}
+int main(){
+	/*#ifndef ONLINE_JUDGE
+     freopen("inputf.txt", "r",stdin);
+     freopen("outputf.txt","w",stdout);
+ #endif
+ */
+	int n;
+	cin>>n;
+	seiveofEratosthenes(n);
+	return 0;
+
+}

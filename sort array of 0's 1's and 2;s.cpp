5a9382298c0204e2d0297a1a98f1765d6d7993f6
Adarsh <42761058+adarsh1github1@users.noreply.github.com>
2020-06-10 21:20:31 +0530

    code edited

--- sort array of 0's 1's and 2;s.cpp
+++ sort array of 0's 1's and 2;s.cpp
@@ -17,6 +17,36 @@ void sortfun(int a[],int n)
    	}
    } 
 }
+void sortfun_easy(int a[],int n){
+	int c1=0,c2=0,c3=0;
+	for(int i=0;i<n;i++){
+		switch(a[i]){
+			case 0:
+				c1++;
+				break;
+			case 1:
+				c2++;
+				break;
+			case 2:
+				c3++;
+				break;
+		}
+	}
+	//cout<<c1<<" "<<c2<<" "<<c3;
+	int j=0;
+	while(c1>0){
+	a[j++] = 0;
+	c1--;
+	}
+	while(c2>0){
+	a[j++] = 1;
+	c2--;
+}
+	while(c3>0){
+	a[j++] = 2;
+	c3--;
+}
+}
 void printl(int arr[],int n){
 	for (int i = 0; i < n; ++i)
 	{
@@ -30,7 +60,8 @@ int main()
 	int a[n];
 	for(int i=0;i<n;i++)
 		cin>>a[i];
-	sortfun(a,n);
+	//sortfun(a,n);
+	sortfun_easy(a,n);
 	printl(a,n);
 	return 0;
 }

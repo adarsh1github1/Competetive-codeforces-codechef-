41141aefb9709782ee7692ead491b3bb2ebaac90
Adarsh <42761058+adarsh1github1@users.noreply.github.com>
2020-06-12 18:26:20 +0530

    code edited .. bug removed

--- set array zeros.cpp
+++ set array zeros.cpp
@@ -1,97 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define N 3 
-#define M 4 
-void setarrayzero(int a[N][M]){
-	int i,j;
-	int n = N;
-	int m = M;
-	int row[n],col[m];
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-			if(a[i][j] == 1){
-				row[i] = 1;
-				col[j] = 1;
-			}
-		}
-	}
-	for(i=0;i<N;i++){
-		for(j=0;j<m;j++){
-			if(row[i] == 1 || col[j] == 1){
-				a[i][j] = 1;
-			}
-		}
-	}
-}
-void setarrayzero_nospace(int a[N][M]){
-	int n = N;
-	int m = M;
-	int i,j;
-	bool rflag,cflag;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			if(i==0 && a[i][j] == 1)
-				rflag = true;
-			if(j==0 && a[i][j] == 1)
-				cflag = true;
-			if(a[i][j] == 1){
-				a[0][i] = 1;
-				a[i][0] = 1;
-			}
-		}
-	}
-	for (int i = 1; i < n; ++i)
-	{
-		/* code */
-		for (int j = 1; j < m; ++j)
-		{
-			/* code */
-			if(a[0][j] == 1 || a[i][0] == 1){
-				a[i][j] = 1;
-			}
-		}
-	}
-	if(rflag ==true){
-	for (int i = 0; i < m; ++i)
-	{
-		a[0][i] = 1;
-	}
-}
-if (cflag == 1)
-{
-	/* code */
-	for (int i = 0; i < n; ++i)
-	{
-		/* code */
-		a[i][0] = 1;
-	}
-}
-}
-void printl(int a[N][M]){
-	int n = N;
-	int m = M;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			cout<<a[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-}
-
-int main()
-{
-	int a[N][M];
-	int i,j;
-	for(i=0;i<N;i++){
-		for(j=0;j<M;j++){
-			cin>>a[i][j];
-		}
-	}
-	setarrayzero(a);
-	printl(a);
-	return 0;
-}
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int i,j;
+        int n= matrix.size();
+        int m = matrix[0].size();
+        bool cflag = false;;
+        bool rflag = false;
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                if(i==0 && matrix[i][j] == 0)
+                    rflag = true;
+                if(j==0 && matrix[i][j] == 0)
+                    cflag = true;
+                
+                if(matrix[i][j] == 0){
+                    matrix[0][j] =0;
+                    matrix[i][0] =0;
+                }
+            }
+        }
+        for(i=1;i<n;i++){
+            for(j=1;j<m;j++){
+                if(matrix[0][j] ==0 || matrix[i][0] == 0)
+                  matrix[i][j] = 0;
+            }
+        }
+        if(rflag == true )
+            for(i=0;i<m;i++)
+                matrix[0][i] = 0;
+        if(cflag == true)
+            for(i=0;i<n;i++)
+                matrix[i][0] = 0;
+        
+    }
+};

22c2db46953278745bb4bf4d498484d4911a089b
Adarsh <42761058+adarsh1github1@users.noreply.github.com>
2020-06-13 16:07:40 +0530

    code updated - method added

--- excel column number leetcode.cpp
+++ excel column number leetcode.cpp
@@ -10,4 +10,11 @@ public:
             }
         return sum;
     }
+    // method - 2 very easy
+    int titleToNumb(string s) {
+	int res = 0;
+	for (char c : s)
+		res = 26 * res + (c - 'A' + 1);
+	return res;
+}
 };
